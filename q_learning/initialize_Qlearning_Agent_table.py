5df0888577bd5370ba8151fecad039eb32388dbd
88-yana <85784752+88-yana@users.noreply.github.com>
2024-01-17 09:02:54 +0900

    初期化の距離の倍率を１にした

--- q_learning/initialize_Qlearning_Agent_table.py
+++ q_learning/initialize_Qlearning_Agent_table.py
@@ -1,31 +1,33 @@
 import numpy as np
+from itertools import product
 
+STEPS = 181
 LR_DIFF = 20
 
 actions = ["Forward", "Left", "Right"]
 action_index = {action: i for i, action in enumerate(actions)}
-states = [(d1, d2, d3) for d1 in range(11) for d2 in range(11) for d3 in range(11)]
+states = list(product(range(0, STEPS, 2), repeat=3))
 state_index = {state: i for i, state in enumerate(states)}
 q_table = np.zeros((len(states), len(actions)))
 
 def Cntl(d_fr, d_lh, d_rh):
-    if d_fr < 1:
+    if d_fr < 20:
         print('判断：止まる', d_fr, d_lh, d_rh)
         return "Stop"
     else:
-        if d_lh > 7 or d_rh < 1:
+        if d_lh > 140 or d_rh < 30:
             print('左に曲がる', d_fr, d_lh, d_rh)
             return "Left"
-        elif d_lh < 4 or (d_fr < d_rh and d_fr < 5):
+        elif d_lh < 80 or (d_fr < d_rh):
             print('右に曲がる', d_fr, d_lh, d_rh)
             return "Right"
         else:
             print('前に進む', d_fr, d_lh, d_rh)
             return "Forward"
 
-for f in range(0,11):
-    for l in range(0,11):
-        for r in range(0,11):
+for f in range(0, STEPS, 2):
+    for l in range(0, STEPS, 2):
+        for r in range(0, STEPS, 2):
             state = (f, l, r)
             d_lr = l - r
             action = Cntl(f, l, r)
--- q_learning/q_learning_agent.py
+++ q_learning/q_learning_agent.py
@@ -1,6 +1,5 @@
 import numpy as np
 import random
-
 from itertools import product
 
 STEPS = 181
